4f8c36d92acdf898ba6c0b989659966ff4ade00f
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-07-10 23:06:17 -0400

    added first 4 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+const busImg = document.querySelector(".intro img");
+busImg.addEventListener("mouseover", () => {
+    busImg.style.transform = "scale(0.8)";
+    busImg.style.transition = "all 0.5s";
+});
+
+busImg.addEventListener("mouseout", () => {
+    busImg.style.transform = "scale(1.0)";
+});
+
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener("dblclick", () => {
+    logo.style.color = "red";
+    logo.style.transform = "scale(1.5)";
+});
+
+const destination = document.querySelectorAll(".destination");
+destination.forEach(element => {
+    element.addEventListener("mouseenter", () => {
+        element.style.color = "rebeccapurple";
+    });
+    element.addEventListener("mouseout", () => {
+        element.style.color = "black";
+    });
+})
